1b8bf842738a3a59a700c07c24872804468abb8b
olima284 <olivialima.student@careerdevs.com>
2022-03-01 20:41:21 -0500

    Initial setup

---
+++ src/main/java/com/careerdevs/JPHAPI/JphapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.JPHAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JphapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JphapiApplication.class, args);
+	}
+
+}
